86b57db8cfcb5720f223757416074c2851c41819
ionut.todoran <Softvision10>
2020-03-04 16:25:12 +0200

    1 St commit

---
+++ src/main/java/com/jpahibernate/otherexample/config/JpaConfig.java
@@ -0,0 +1,35 @@
+package com.jpahibernate.otherexample.config;
+
+import org.hibernate.boot.model.naming.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@EnableJpaRepositories
+@Configuration
+@EnableSwagger2
+public class JpaConfig {
+    @Bean
+    public PhysicalNamingStrategy physical() {
+        return new PhysicalNamingStrategyStandardImpl();
+    }
+
+    @Bean
+    public ImplicitNamingStrategy implicit() {
+        return new ImplicitNamingStrategyLegacyJpaImpl();
+    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
